fa61f777303852798c65ac25b141ad4c24510cc5
Christian Pinto <pintochristian2@gmail.com>
2019-11-03 05:19:19 -0500

    dropdown menu and css dimensions

--- FirstScene/screens/HomeScreen.js
+++ FirstScene/screens/HomeScreen.js
@@ -11,11 +11,16 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
+import { Dimensions } from "react-native";
+
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import DropdownMenu from 'react-native-dropdown-menu';
 
 class HomeScreen extends React.Component{
   render(){
+    const data = [["EMS Department", "Fire Department", "Police Department"]]
+    
     return (
       <View style={styles.container}>
         <ScrollView
@@ -26,13 +31,28 @@ class HomeScreen extends React.Component{
           </View>
 
           <TextInput
-            style={{textAlign:"center", marginBottom: 5, height: 40, width: 300 ,borderColor: 'gray', borderWidth: 1 }}
+            style={styles.input}
             onChangeText={text => onChangeText(text)}
-            value=""
+            placeholder="Name"
+          />
+            <DropdownMenu
+              style={{flex: 1}}
+              bgColor={'white'}
+              tintColor={'teal'}
+              activityTintColor={'green'}
+              data={data}
+            ></DropdownMenu>
+            <View style={{height: 120}} />
+
+          <TextInput
+            style={styles.input}
+            onChangeText={text => onChangeText(text)}
+            placeholder="Badge Number"
           />
 
           <Button
-          title="Go to Details"
+          style= {{}}
+          title="Login"
           onPress={() => this.props.navigation.navigate('Incidents')}
         />
         </ScrollView>
@@ -57,90 +77,26 @@ const AppNavigator = createStackNavigator({
 
 export default createAppContainer(AppNavigator);
 
+var width = Dimensions.get('window').width
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
   appName: {
-    width: 250,
+    width: width,
     height: 100,
     fontSize: 40,
     textAlign: 'center'
   },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+  input: {
+    textAlign:"center", 
+    marginBottom: 5, 
+    height: 40, 
+    width: (width * .75) ,
+    borderColor: 'gray', 
+    borderWidth: 1, 
+    marginLeft: 30 
+  }
 });
